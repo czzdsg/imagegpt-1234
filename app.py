118b39d3b49ed6b23a39843da167686f97dfbf01
czzdsg <117070553+czzdsg@users.noreply.github.com>
2023-07-29 16:10:57 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+from flask import Flask, request, render_template
+import json,time
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        q=request.form.get("question")
+        body=json.dumps(
+            {
+                "version":"db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+                "input":{"prompt":q}
+
+            }
+
+
+        )
+        headers={
+            "Authorization":"Token r8_L3enTrYJWfsq2b2dwNWzksOA39JNC8S1s6cxw",
+            "Content-Type":"application/json"
+        }
+        r=requests.post("https://api.replicate.com/v1/predictions",data=body,headers=headers)
+        time.sleep(10)
+        get_url=r.json()["urls"]["get"]
+        get_result=requests.post(get_url,headers=headers).json()["output"]
+        result=get_result[0]
+        return(render_template("index.html",result=get_result[0]))
+    else:
+        return render_template("index.html", result="waiting")
+
+if __name__ == "__main__":
+    app.run()
+    
+
